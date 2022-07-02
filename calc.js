0cd4e401120a0bc06a24c944b69307a90f69c5be
zachary sibson <70235818+zsibson@users.noreply.github.com>
2022-07-02 17:08:07 -0600

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,39 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+
+
+reader.question("What would you like to calculate?", function(input){
+	const tokens = input.split(' ');
+	
+	const mathSymbol = tokens[0];
+	const num1 = Number(tokens[1]);
+	const num2 = Number(tokens[2]);
+
+	if (mathSymbol === "+") {
+		console.log(num1 + num2); 	
+	}
+	if (mathSymbol === "-") {
+		console.log(num1 - num2);
+	}
+	if (mathSymbol === "*") {
+		console.log(num1 * num2);
+	}
+	if (mathSymbol === "/") {
+		console.log(num1 / num2);
+	}
+	if (mathSymbol === "sqrt") {
+		console.log(Math.sqrt(num1));
+	}
+
+	// This line closes the connection to the command line interface.
+	reader.close()
+
+});
+
+
